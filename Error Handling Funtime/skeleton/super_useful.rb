3598d8d15df6e9685c9a873e328f4d2c7409e627
Daniel Mechenko <dmechenko@gmail.com>
2021-07-07 14:29:18 -0400

    Finished ErrorFuntime

--- Error Handling Funtime/skeleton/super_useful.rb
+++ Error Handling Funtime/skeleton/super_useful.rb
@@ -1,3 +1,9 @@
+require "byebug"
+class NonFruitError < StandardError
+  def say_something
+    raise "This ain't a fruit!"
+  end
+end
 # PHASE 2
 def convert_to_int(str)
   Integer(str)
@@ -7,19 +13,28 @@ end
 FRUITS = ["apple", "banana", "orange"]
 
 def reaction(maybe_fruit)
-  if FRUITS.include? maybe_fruit
+  #debugger
+  if FRUITS.include?(maybe_fruit)
     puts "OMG, thanks so much for the #{maybe_fruit}!"
   else 
-    raise StandardError 
+    raise NonFruitError
   end 
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  begin 
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit)
+  rescue NonFruitError => e
+    if maybe_fruit == "coffee"
+      retry 
+    else
+      e.say_something
+    end
+  end
 end  
 
 # PHASE 4
@@ -31,7 +46,7 @@ class BestFriend
   end
 
   def talk_about_friendship
-    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+    puts "Wowza, we've been friends for #{@yrs_known} year(s). Let's be friends for another #{1000 * @yrs_known} years."
   end
 
   def do_friendstuff
